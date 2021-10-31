865d78f3d5d1927cc35871aaca8a8766980279ff
David Resin <david.resin@epfl.ch>
2021-10-31 15:21:18 -0400

    Buffering progress + small optimization

--- heightmap/helpers/functions.py
+++ heightmap/helpers/functions.py
@@ -1,32 +1,35 @@
+import json
 import numpy as np
 import pyperclip as pc
 
 from itertools import product
 from scipy.ndimage.measurements import label
 
-def make_affine(x0_old, x0_new, x1_old, x1_new):
+# Computes the affine coefficients of two 2D points
+def getMH(x0_old, x0_new, x1_old, x1_new):
     m = (x0_new - x1_new) / (x0_old - x1_old)
     h = (x1_new * x0_old - x0_new * x1_old) / (x0_old - x1_old)
     
     return m, h
 
-def make_affine_matrix_1d(m, h):
-    return np.array([
-        [m, h],
-        [0, 1]
-    ])
+# Makes an affine matrix from a list of Ms and a list of Hs
+def make_affine_matrix(ms, hs):
+    if len(ms) != len(hs):
+        raise ValueError("Ms and Hs must have the same length!")
 
-def make_affine_matrix_2d(mX, mZ, hX, hZ):
-    return np.array([
-        [mX, 0, hX],
-        [0, mZ, hZ],
-        [0,  0,  1]
-    ])
+    M = np.diag(ms)
+    M = np.vstack([M, np.zeros(len(ms))])
+    H = np.hstack([hs, 1])
+    H = H.reshape((H.shape[0], 1))
 
+    return np.hstack([M, H])
+
+# Transforms a whole set of points with a given matrix
 def convert_points(points, mat):
     points = np.c_[ points, points.shape[0]]
     return np.matmul(mat, points)
 
+# Returns the altitude of a point defined by its latitude and longitude
 def getV(rasters, lon, lat):
     r = find_raster(rasters, lon, lat)
     
@@ -35,15 +38,18 @@ def getV(rasters, lon, lat):
     
     return r.value_at_coords(lon, lat)
 
-def mapTopLeft(mX, mZ):
-    return mX * 128 - 64, mZ * 128 - 64
+# Returns in-game X and Z coordinates of the top-left-most point of a map given by its XZ map indices
+def mapTopLeft(mapX, mapZ):
+    return mapX * 128 - 64, mapZ * 128 - 64
 
+# Tells whether a longitude and a latitude fit within a given raster
 def fits_bb(raster, lon, lat):
     bb = raster.bounds
     lonFit = bb.left <= lon <= bb.right
     latFit = bb.bottom <= lat <= bb.top
     return lonFit, latFit
 
+# Finds a raster that matches a longitude and a latitude
 def find_raster(rasters, lon, lat):
     for r in rasters:
         if sum(fits_bb(r, lon, lat)) == 2:
@@ -51,10 +57,12 @@ def find_raster(rasters, lon, lat):
         
     return None
 
+# Transforms a single point with a given matrix
 def xzTrans(m2c, x, z):
     trans = np.matmul(m2c, [x, z, 1])
     return tuple(trans[:2])
     
+# Takes two XZ points and returns the list of sub-areas with their corresponding raster
 def raster_ranges(rasters, m2c, c2m, xS, zS, xE, zE):
     points = [(xS, zS)]
     ranges = []
@@ -85,6 +93,7 @@ def raster_ranges(rasters, m2c, c2m, xS, zS, xE, zE):
             
     return ranges
             
+# Generates a 128x128 Minecraft heightmap from a given top-left-most XZ point
 def gen_heightmap(rasters, m2c, c2m, v2y, xS, zS):
     xE, zE = xS + 127, zS + 127
     ranges = raster_ranges(rasters, m2c, c2m, xS, zS, xE, zE)
@@ -130,6 +139,7 @@ def gen_heightmap(rasters, m2c, c2m, v2y, xS, zS):
     
     return np.around(ret).T.astype(int)
 
+# Generates a fill command string
 def gen_fill(strings, edges, Y, block0, mode=None, block1=None):
     h, w, X0, Z0 = edges
     X1 = X0 + h
@@ -154,6 +164,7 @@ def gen_fill(strings, edges, Y, block0, mode=None, block1=None):
     cmd = strings["fill_command"].format(X0, Y, Z0, X1, Y, Z1, block0, mode, block1).strip()
     return strings["template"].format(cmd)
             
+# Generates a clone command string
 def gen_clone(strings, edges, Y0, yShift, mode, block=None):
     h, w, X0, Z0 = edges
     X1 = X0 + h
@@ -177,6 +188,7 @@ def gen_clone(strings, edges, Y0, yShift, mode, block=None):
     cmd = strings["clone_command"].format(X0, Y0, Z0, X1, Y0, Z1, X0, Y1, Z0, mode, block).strip()
     return strings["template"].format(cmd)
 
+# Packages command for entry into a command block, yields several sets if too long
 def package_commands(cmds, strings):
     max_chars = 32500
     prefix = strings["prefix"]
@@ -204,17 +216,8 @@ def package_commands(cmds, strings):
         input("Press Enter to store the next command in your clipboard...")
         pc.copy(b)
         print(f"Batch {i + 1} of {len(batches)}")
-        
-def thicken_old(multilayer):
-    ret = np.zeros(multilayer.shape, dtype=bool)
-    mem = np.zeros(multilayer.shape[:2], dtype=bool)
-    
-    for z in range(multilayer.shape[2]):
-        ret[..., z] = (multilayer[..., z] > 0) | mem
-        mem = ret[..., z]
-        
-    return ret[..., ::-1]
 
+# Turns a height array into a series of binary arrays for each layer
 def thicken(arr):
     bottom = np.min(arr)
     top = np.max(arr)
@@ -225,7 +228,8 @@ def thicken(arr):
         ret[..., d] = (arr == bottom + d)
 
     return ret, bottom, depth
-        
+
+# Splits a single layer into multiple sublayers of individual connected components
 def split_layer(layer):
     structure = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
     labeled, N = label(layer, structure)
@@ -242,20 +246,19 @@ def split_layer(layer):
         j0, j1 = tuple(np.argwhere(np.any(component, axis=0))[[0, -1]].flatten().tolist())
         i0, i1 = tuple(np.argwhere(np.any(component, axis=1))[[0, -1]].flatten().tolist())
         
-        ret.append((i1 - i0, j1 - j0, i0, j0))
+        ret.append((component[i0:i1 + 1, j0:j1 + 1], i0, j0))
         
     return ret
 
-def tesselate_layer(layer, h0, w0, i0, j0, xShift, zShift):
-    #Trivial case
-    if len(np.unique(layer)) == 1:
-        return [(h0, w0, i0, j0)]
-    
-    # Slice out the wanted part
-    sub_layer = layer[i0:i0 + h0 + 1, j0:j0 + w0 + 1]
+# Finds a near-optimal rectangular tiling of a layer
+def tesselate_layer(sub_layer, i0, j0, xShift, zShift):
+    # Trivial case
+    if len(np.unique(sub_layer)) == 1:
+        h0, w0 = sub_layer.shape
+        return [(h0 - 1, w0 - 1, i0, j0)]
     
     # Initialize working arrays
-    possible =   gen_tesseract_opt(sub_layer)
+    possible =   gen_tesseract(sub_layer)
     selected =   np.zeros(possible.shape)
     mask =       gen_mask(possible)
     coords_gen = gen_coords_generator(possible)
@@ -275,6 +278,75 @@ def tesselate_layer(layer, h0, w0, i0, j0, xShift, zShift):
     
     return [(h, w, i + i0 + xShift, j + j0 + zShift) for h, w, i, j in np.column_stack(selected.nonzero()).tolist()]
 
+old = """        
+# Splits a single layer into multiple sublayers of individual components
+def split_layer_old(layer):
+    structure = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
+    labeled, N = label(layer, structure)
+    background_label = None if N == 1 else labeled[tuple(np.column_stack(np.where(layer == 0))[0])]
+    ret = []
+    
+    for idx in range(1, N + 1):
+        if background_label is None:
+            idx = layer[0, 0]
+        elif background_label == idx:
+            continue
+            
+        component = (labeled == idx)
+        j0, j1 = tuple(np.argwhere(np.any(component, axis=0))[[0, -1]].flatten().tolist())
+        i0, i1 = tuple(np.argwhere(np.any(component, axis=1))[[0, -1]].flatten().tolist())
+        
+        ret.append((i1 - i0, j1 - j0, i0, j0))
+        
+    return ret
+    
+def tesselate_layer_old(layer, h0, w0, i0, j0, xShift, zShift):
+    # Trivial case
+    if len(np.unique(layer)) == 1:
+        return [(h0, w0, i0, j0)]
+    
+    # Slice out the wanted part
+    sub_layer = layer[i0:i0 + h0 + 1, j0:j0 + w0 + 1]
+    
+    # Initialize working arrays
+    possible =   gen_tesseract(sub_layer)
+    selected =   np.zeros(possible.shape)
+    mask =       gen_mask(possible)
+    coords_gen = gen_coords_generator(possible)
+
+    # Iteratively select the best fills
+    while select_next_fill(selected, possible, coords_gen, mask, sub_layer):
+        pass
+
+    # Generate scores
+    scores = gen_scores_post(selected)
+
+    # Remove redundant fills
+    sanitize(selected, scores)
+
+    # Shrink fills to their minimum useful size
+    shrink(selected, scores)
+    
+    return [(h, w, i + i0 + xShift, j + j0 + zShift) for h, w, i, j in np.column_stack(selected.nonzero()).tolist()]
+    
+def cubify_old(arr, strings, shift=(0, 0)):
+    multilayer, Y0, depth = thicken(arr.T)
+    xShift, zShift = shift
+    cmds = []
+    
+    for y in range(depth):
+        layer = multilayer[..., y]
+        sublayers = split_layer_old(layer)
+        edges = []
+        
+        for (h, w, i, j) in sublayers:
+            edges += tesselate_layer_old(layer, h, w, i, j, xShift, zShift)
+            
+        cmds += [gen_fill(strings, elem, Y0 + y, "diamond_block", mode="keep") for elem in edges]
+    
+    package_commands(cmds, strings)
+
+# Turns a Minecraft heightmap into a set of individual commands yielded as batches
 def cubify(arr, strings, shift=(0, 0)):
     multilayer, Y0, depth = thicken(arr.T)
     xShift, zShift = shift
@@ -285,13 +357,35 @@ def cubify(arr, strings, shift=(0, 0)):
         sublayers = split_layer(layer)
         edges = []
         
-        for (h, w, i, j) in sublayers:
-            edges += tesselate_layer(layer, h, w, i, j, xShift, zShift)
+        for (sub_layer, i, j) in sublayers:
+            edges += tesselate_layer(sub_layer, i, j, xShift, zShift)
             
-        cmds += [gen_fill(strings, elem, Y0 + y, "diamond_block", mode="replace", block1="air") for elem in edges]
+        cmds += [gen_fill(strings, elem, Y0 + y, "diamond_block", mode="keep") for elem in edges]
+    
+    package_commands(cmds, strings)
+"""
+    
+# Turns a Minecraft heightmap into a set of individual commands yielded as batches
+def cubify(arr, strings, shift=(0, 0)):
+    multilayer, Y0, depth = thicken(arr.T)
+    xShift, zShift = shift
+    cmds = []
+    
+    for y in range(depth):
+        layer = multilayer[..., y]
+        sublayers = split_layer(layer)
+        edges = []
+        
+        for (sub_layer, i, j) in sublayers:
+            edges += tesselate_layer(sub_layer, i, j, xShift, zShift)
+            
+        cmds += [gen_fill(strings, elem, Y0 + y, "diamond_block", mode="keep") for elem in edges]
     
     package_commands(cmds, strings)
     
+    return cmds, (Y0, Y0 + depth)
+    
+# Extracts the border of a binary component, array edges included
 def gen_border(arr):
     h, w = arr.shape
     edge = np.ones((h, w), dtype=bool)
@@ -306,15 +400,7 @@ def gen_border(arr):
         
     return (mem + edge) * (arr > 0)
 
-def gen_bound_mask(arr):
-    h, w = arr.shape
-    ret = np.zeros((h, w, h, w), dtype=bool)
-    
-    for i, j in np.column_stack((1 - arr).nonzero()):
-        set_block_at(ret, i, j, True)
-        
-    return ret
-
+# Generate mask of possible tesseract values
 # More performant than gen_tesseract(np.ones(tsrct.shape[:2]))
 def gen_mask(tsrct):
     h, w = tsrct.shape[:2]
@@ -328,7 +414,8 @@ def gen_mask(tsrct):
 
     return mask
 
-def gen_tesseract_opt_sub(arr, true_if_and):
+# Subroutine for gen_tesseract
+def gen_tesseract_sub(arr, true_if_and):
     h, w = arr.shape
     ret = np.zeros((h, w, h, w), dtype=bool)
     ret[0, 0] = arr > 0
@@ -347,26 +434,31 @@ def gen_tesseract_opt_sub(arr, true_if_and):
         
     return ret
 
-def gen_tesseract_opt(arr, carve=True):
+# Generates a tesseract for a given array, and ignores values that do not touch an edge if carve is set to True
+def gen_tesseract(arr, carve=True):
     border = gen_border(arr)
-    ret = gen_tesseract_opt_sub(arr, True)
+    ret = gen_tesseract_sub(arr, True)
     
     if carve:
-        no_fillers = gen_tesseract_opt_sub(border, False)
+        no_fillers = gen_tesseract_sub(border, False)
         ret = ret & no_fillers
         
     return ret
 
+# Alters a tesseract at a given coordinate and updates the score array
 def modify_fill_at(tsrct, scores, h, w, i, j, true_if_add):
     tsrct[h, w, i, j] = true_if_add
     scores[i:i + h + 1, j:j + w + 1] -= -1 if true_if_add else 1
 
+# Removes a fill
 def remove_fill_at(tsrct, scores, h, w, i, j):
     modify_fill_at(tsrct, scores, h, w, i, j, False)
     
+# Adds a fill
 def add_fill_at(tsrct, scores, h, w, i, j):
     modify_fill_at(tsrct, scores, h, w, i, j, True)
     
+# Iterates the given generator until a value is given that is still valid
 def yield_coords(possible, gen):
     try:
         while True:
@@ -377,6 +469,7 @@ def yield_coords(possible, gen):
     except StopIteration:
         return None
 
+# Makes a generator of valid values out of a tesseract
 def gen_coords_generator(possible):
     h, w = possible.shape[:2]
     order = sorted(np.column_stack(possible.nonzero()), key=lambda elem: (-max(elem[:2]), -elem[0], -elem[1]))
@@ -384,6 +477,7 @@ def gen_coords_generator(possible):
     for h0, w0, i0, j0 in order:
         yield h0, w0, i0, j0
     
+# Selects the next valid fill
 def select_next_fill(selected, possible, gen, mask, arr):
     H, W = arr.shape
     coords = yield_coords(possible, gen)
@@ -401,6 +495,7 @@ def select_next_fill(selected, possible, gen, mask, arr):
     
     return True
     
+# Creates a score array
 def gen_scores_post(selected):
     ret = np.zeros(selected.shape[:2], dtype=np.int16)
     
@@ -410,11 +505,13 @@ def gen_scores_post(selected):
         
     return ret
 
+# Removes strictly redundant fills
 def sanitize(selected, scores):
     for h, w, i, j in sorted(np.column_stack(selected.nonzero()), key=lambda t: (-np.prod(t[:2]), -t[0])):
         if np.all(scores[i:i + h + 1, j:j + w + 1] > 1):
             remove_fill_at(selected, scores, h, w, i, j)
-            
+        
+# Shrinks all fills to their absolute minimal size
 def shrink(selected, scores):
     for h, w, i, j in sorted(np.column_stack(selected.nonzero()), key=lambda t: (-np.prod(t[:2]), -t[0])):
         vals = scores[i:i + h + 1, j:j + w + 1] == 1
@@ -430,8 +527,18 @@ def shrink(selected, scores):
             if newH < h or newW < w:
                 remove_fill_at(selected, scores, h, w, i, j)
                 add_fill_at(selected, scores, newH, newW, i + iMin, j + jMin)
-                
+
+old = """
+# Yields commands for a given map coordinate
 def yield_map(rasters, strings, m2c, c2m, v2y, xMap, zMap):
     xS, zS = mapTopLeft(xMap, zMap)
     arr = gen_heightmap(rasters, m2c, c2m, v2y, xS, zS)
     cubify(arr, strings, shift=(xS, zS))
+"""
+    
+# Yields commands for a given map coordinate
+def yield_map(rasters, strings, m2c, c2m, v2y, xMap, zMap):
+    xS, zS = mapTopLeft(xMap, zMap)
+    arr = gen_heightmap(rasters, m2c, c2m, v2y, xS, zS)
+    cmds, Y = cubify(arr, strings, shift=(xS, zS))
+    save_range((xMap, zMap), cmds, Y)
